03e3c71d517db9c1b4d032089550ac81954bf2c0
Fy <fuyu0824@pku.edu.cn>
2020-03-09 10:41:04 +0800

    修正app_secret键名

--- service/user_openid_service.go
+++ service/user_openid_service.go
@@ -13,9 +13,9 @@ type UserOpenIdService struct {
 	Code string `form:"code" json:"code"`
 }
 
-// gecode 用户登录函数，获取openidhesessionkey，作为之后操作的验证
+// gecode 用户登录函数，获取openid和sessionkey，作为之后操作的验证
 func (service *UserOpenIdService) GetCode(c *gin.Context) serializer.Response {
-	res, err := weapp.Login(os.Getenv("APP_ID"), os.Getenv("APP_SECREAT"), service.Code)
+	res, err := weapp.Login(os.Getenv("APP_ID"), os.Getenv("APP_SECRET"), service.Code)
 
 	if err != nil {
 		//处理错误
