393cff28c63de40094d68ea2e052ca2f52a3b12d
hua <1901210745@pku.edu.cn>
2020-03-10 17:30:02 +0800

    删除之前定义个user相关的代码

--- conf/conf.go
+++ conf/conf.go
@@ -14,15 +14,4 @@ func Init() {
 
 	//连接数据库
 	model.Database(os.Getenv("MYSQL_DSN"), os.Getenv("MYSQL_DSN2"))
-
-	//判断该用户这天是否已经提交过
-	count := 0
-	if model.DB.Model(&model.Corp{}).Where("corpid = 100000001").Count(&count); count == 0 {
-		model.DB.Create(&model.Corp{
-			Corpid:       "100000001",
-			Corpname:     "北京大学",
-			TypeCorpname: "学校名称",
-			TypeUsername: "学号",
-		})
-	}
 }
